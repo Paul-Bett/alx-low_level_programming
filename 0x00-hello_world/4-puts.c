e21d719939f084161fdb9f9af3d386b30383f5fe
Paul-Bett <bettpaul6@gmail.com>
2022-06-01 23:19:15 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+*main - How to use Puts function
+*/
+int main(void)
+{
+	char phrase = "Programming is like building a multilingual puzzle";
+	puts(phrase);
+	return (0);
+}
