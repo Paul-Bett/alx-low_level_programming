39e2ceeae133defd1faaa0207c02e19d3ccb6c6f
Paul-Bett <bettpaul6@gmail.com>
2022-06-02 06:22:36 -0700

    print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59);
 	return (0);
 }
