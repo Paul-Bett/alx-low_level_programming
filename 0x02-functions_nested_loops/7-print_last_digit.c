483af5cca4f6114902205d2524fc098646d3993b
Paul-Bett <bettpaul6@gmail.com>
2022-06-07 11:55:53 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_last_digit - Begin here
+*@n: Integer number
+*Return: Value 0
+*/
+
+int print_last_digit(int n);
+{	
+	_putchar((n %= 10) + '0');
+	}
+	_putchar('\n');
+	return (0);
+}
