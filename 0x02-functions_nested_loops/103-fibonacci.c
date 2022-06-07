ba9622e208dbe7507fcbf5bd1ebb89e52ea91e5d
Paul-Bett <bettpaul6@gmail.com>
2022-06-07 14:14:46 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - first 50 fibonacci numbers
+* multiplies of 3 or 5 and sum of the multiples
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+	long int sum =0;
+	long int n1 = 0;
+	long int n2 = 1;
+
+	while (sum <= 4000000)
+	{
+		sum = n1 + n2;
+		if (sum % 2 == 0)
+		{
+			pringf("%ld", sum);
+			if (sum < 4000000)
+				printf(", "); 
+		}
+		n1 = n2;
+		n2 = sum;
+	}
+	printf("\n");
+	return (0);
+}
