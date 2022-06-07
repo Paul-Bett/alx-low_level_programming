e543907834939362c7bf6557a6a59e638bd34bfe
Paul-Bett <bettpaul6@gmail.com>
2022-06-07 11:13:26 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*_print_sign - Begin Here
+*@n: integer value
+*return 1 if lower case and 0 otherwise
+*Return: Value 0 (true)
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar("+");
+	}
+	else if (n = 0)
+	{
+		return (0);
+		_putchar("0");
+	}
+	else
+	{
+		return (-1);
+		_putchar("1");
+	}
+}
