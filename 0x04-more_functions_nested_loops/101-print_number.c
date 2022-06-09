66491f0d6c09effc0b01de09a047138fbedd32a0
Paul-Bett <bettpaul6@gmail.com>
2022-06-09 15:44:09 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,44 +14,26 @@ void print_number(int n)
 		n *= -1;
 	}
 	if (n < 10)
-	{
 		_putchar(n + '0');
-	}
 	else if (n < 100)
 	{
-		ones = n % 10;
-		tens = n / 10;
+		ones = n % 10, tens = n / 10;
 		_putchar(tens + '0');
 		_putchar(ones + '0');
 	}
 	else if (n < 1000)
 	{
-		ones = n % 10;
-		tens = (n / 10) % 10;
-		hundreds = n / 100;
+		ones = n % 10, tens = (n / 10) % 10, hundreds = n / 100;
 		_putchar(hundreds + '0');
 		_putchar(tens + '0');
 		_putchar(ones + '0');
 	}
 	else if (n < 10000)
 	{
-		ones = n % 10;
+		ones = n % 10,
 		tens = (n / 10) % 10;
-		hundreds = (n / 100) % 10;
+		hundreds = (n / 100) % 10,
 		thousands = n / 1000;
-		_putchar(thousands + '0');
-		_putchar(hundreds + '0');
-		_putchar(tens + '0');
-		_putchar(ones + '0');
-	}
-	else if (n < 100000)
-	{
-		ones = n % 10;
-		tens = (n / 10) % 10;
-		hundreds = (n / 100) % 10;
-		thousands = (n / 1000) % 10;
-		tenthous = n / 10000;
-		_putchar(tenthous + '0');
 		_putchar(thousands + '0');
 		_putchar(hundreds + '0');
 		_putchar(tens + '0');
