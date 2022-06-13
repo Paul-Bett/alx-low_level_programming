64024e97982faf9fd78f09c6454af89fb80d1305
Paul-Bett <bettpaul6@gmail.com>
2022-06-13 08:58:37 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
-* puts2 - Use pointer to change value in memory
-* @s: Pointer to string location
+* _puts - Use pointer to change value in memory
+* @str: Pointer to string location
 *Return: Null/Void
 */
-void puts2(char *s)
+void _puts(char *str)
 {
-	int i = 0;
+	char *cp = str
+	int count;
 
-	while (*s)
+	while (*cp)
 	{
-	i++;
-	s++;
+		count++;
+		cp++;
 	}
-	s -= (i + 1);
-	for (; i > 0; i++)
+	for (i = 0; i < count / 2; i++)
 	{
-		putchar(*s);
-		s += 2;
+		putchar(*str);
+		str +=2;
 	}
 	putchar('\n');
 }
