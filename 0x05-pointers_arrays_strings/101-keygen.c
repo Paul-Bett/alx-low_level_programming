51d33256fd8ec6e624e686a5fd914c8b0f70c86b
Paul-Bett <bettpaul6@gmail.com>
2022-06-14 04:59:49 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,13 +9,16 @@ int main(void)
 {
 	int r = 0, c = 0;
 	time_t t;
+	int sum = 0;
 
 	srand((unsigned) time(&t));
 	while (c < 15)
 	{
 		r = rand() % 128;
 		c++;
-		printf("%c", r);
+/*		printf("%c", r);*/
+		sum += r;
 	}
+	printf("%c", sum);
 	return (0);
 }
