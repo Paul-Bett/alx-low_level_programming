7a6b1e8d86cdd7a07b54ac2fadaee5f1aa57742f
Paul-Bett <bettpaul6@gmail.com>
2022-06-16 09:46:12 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,10 +10,13 @@ char *cap_string(char *n)
 	int i;
 
 	i = 0;
+	if (n[0] >= 'a' && n[0] <= 'z')
+		n[0] += ('A' - 'a');
 	while (n[i])
 	{
-		if (n[i] == ' ' || n[i] == '\n' || n[0])
+		if (n[i] == ' ' || n[i] == '\n')
 		{
+		i++;
 			if (n[i] >= 'a' && n[i] <= 'z')
 				n[i] += ('A' - 'a');
 		}
