e967c952d59d9dfd8b8eea237fd99ac7be4c4e84
Paul-Bett <bettpaul6@gmail.com>
2022-06-16 06:17:23 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * _strcmp - compare string values
  * @s1: input value
  * @s2: input value
  *
- * Return: s1[i] - s2[i]
+ * Return: s1[i] - s2[i] if false and - 0 if true
  */
 int _strcmp(char *s1, char *s2)
 {
