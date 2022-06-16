8c32dc3e1ba55a20b55dcc4793e82e6b16035b39
Paul-Bett <bettpaul6@gmail.com>
2022-06-16 05:39:08 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (n = 0)
+	{
+		dest[i] = src[i];
+		i++;
+		n--;
+	}
+	return (dest);
+}
