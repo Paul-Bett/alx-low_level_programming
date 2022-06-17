be6374c009228425b685485f57ac330dfeab6bca
Paul-Bett <bettpaul6@gmail.com>
2022-06-17 10:09:53 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -13,15 +13,10 @@ void print_number(int n)
 	int out;
 
 	len = 0;
-	if (n < 0)
-	{
-		_putchar('-');
-		n *= -1;
-	}
 	i = n;
-	if (n < 10)
+	if (n = 0)
 	{
-		_putchar(n + '0');
+		_putchar('0');
 	}
 	else
 	{
