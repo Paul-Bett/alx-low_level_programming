dc994b2f83b1690e9cf1051ffe234bc8fe55cde0
Paul-Bett <bettpaul6@gmail.com>
2022-06-20 01:30:26 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset - fills n bytes of the memory area pointed by s with the constant byte b
+ * _memset - fills n bytes of the memory pointed by s with the byte b
  * @s: Pointer to array location
  * @b: Constant byte to replace sotred date
  * @n: number of bytes to be replaced
@@ -9,7 +9,8 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
-	for (i = 0; i <= n; i++)
+
+	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
 	}
