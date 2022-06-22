fc7492449a034c177f346c4d03d004ab54358e69
Paul-Bett <bettpaul6@gmail.com>
2022-06-22 07:28:39 -0700

    Find Squaer root Recursive loop

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+/**
+ * sqrt_tracer - Guess rangom numbers to get square root
+ * @n: Integer Value
+ * @min: Lower guess constraint
+ * @max: Upper guess constraint
+ * Return: Square root
+ */
+int sqrt_tracer(int n, int min, int max)
+{
+	if (max < min)
+		return (-1);
+	int guess = (min + max) / 2;
+	int guessans = guess * guess;
+
+	if (guessans == n)
+		return (guess);
+	else if (guessans < n)
+		sqrt_tracer(n, guess + 1, max);
+	else
+		sqrt_tracer(n, min, guess - 1);
+}
+
+/**
+ * _sqrt_recursion - Performs square root of a number
+ * @n: Integer Value
+ * Return: Square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_tracer(n, 1, n));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,9 @@
+/**
+ *  is_prime_number - Checks if prime number
+ * @n: Integer Value
+ * Return: 1 if true 0 otherwise
+ */
+int  is_prime_number(int n)
+{
+	if (
+}
