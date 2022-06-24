2a9a8453920beb07becf92d6de604e37b2cec664
Paul-Bett <bettpaul6@gmail.com>
2022-06-24 06:24:48 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main -  Prints number of arguements
+* @argc: arguement count
+* @argv: array of pointers to CLI arguement
+* Return: 0
+**/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", (atoi(argv[argc - 1]) * atoi(argv[argc -2])));
+	return (0);
+}
