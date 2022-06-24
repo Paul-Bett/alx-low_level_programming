6d9590b983ad82406660fe6744adfc51dfb9b265
Paul-Bett <bettpaul6@gmail.com>
2022-06-24 06:27:17 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,6 +8,6 @@
 **/
 int main(int argc, char *argv[])
 {
-	printf("%d\n", (atoi(argv[argc - 1]) * atoi(argv[argc -2])));
+	printf("%d\n", (atoi(argv[argc - 1]) * atoi(argv[argc - 2])));
 	return (0);
 }
