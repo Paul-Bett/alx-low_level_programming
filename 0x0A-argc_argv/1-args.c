5020096da196f3de16eee879b096f55f7582d188
Paul-Bett <bettpaul6@gmail.com>
2022-06-24 06:02:21 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main -  Prints number of arguements
+* @argc: arguement count
+* @argv: array of pointers to CLI arguement
+* Return: 0
+**/
+int main(int argc, char *argv[])
+{
+	*argv = *argv;
+	return (argc - 1);
+}
