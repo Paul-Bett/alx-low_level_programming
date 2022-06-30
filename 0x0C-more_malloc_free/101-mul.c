795cac443562f103650ba0fa5c8aecb3bdc03ad4
Paul-Bett <bettpaul6@gmail.com>
2022-06-30 23:51:45 +0300

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,11 +9,11 @@
 int main(int argc, char *argv[])
 {
 	if (argc <= 2 || argc > 3)
-		{
-			printf("%s\n", "Error");
-				return (98);
-		}
-	else
+	{
+		printf("%s\n", "Error");
+			return (98);
+	}
+	else if ((atoi(argv[argc - 1]) * atoi(argv[argc - 2])) > 0)
 	{
 		printf("%d\n", (atoi(argv[argc - 1]) * atoi(argv[argc - 2])));
 		return (0);
