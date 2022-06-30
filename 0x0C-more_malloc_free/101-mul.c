05231028e1b716dd2432f5462557972b3787b393
Paul-Bett <bettpaul6@gmail.com>
2022-06-30 23:47:24 +0300

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main -  Prints mul of arguements
+* @argc: arguement count
+* @argv: array of pointers to CLI arguement
+* Return: 0
+**/
+int main(int argc, char *argv[])
+{
+	if (argc <= 2 || argc > 3)
+		{
+			printf("%s\n", "Error");
+				return (98);
+		}
+	else
+	{
+		printf("%d\n", (atoi(argv[argc - 1]) * atoi(argv[argc - 2])));
+		return (0);
+	}
+	printf("%s\n", "Error");
+	return (98);
+}
