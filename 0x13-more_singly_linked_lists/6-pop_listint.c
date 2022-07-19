a75b292793425effd7fbeb77aafe3b0b6948d2ec
Paul-Bett <bettpaul6@gmail.com>
2022-07-19 19:10:35 +0300

    6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * pop_listint - pops off the head of the list and returns its contents
+ * @head: head of the list
+ * Return: contents of head
+ */
+int pop_listint(listint_t **head)
+{
+	int pop;
+	listint_t *current;
+
+	if (head == NULL)
+		return (0);
+	current = *head;
+	pop = current->n;
+	*head = current->next;
+	return (pop);
+}
