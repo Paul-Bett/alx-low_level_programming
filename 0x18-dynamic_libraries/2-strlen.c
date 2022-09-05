eae312773a4e957f257737a4300c89c1bbc4656b
root <root@DESKTOP-TPQ0LCA.localdomain>
2022-09-05 22:53:19 +0300

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - Calculates the length of a string
+ * @s: string
+ * Descripion: Calculates the lenght of a string
+ * Return: An integer representing the lenght of a string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; s[len]; len++)
+	{}
+
+	return (len);
+}
